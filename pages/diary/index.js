4867f8068769506bfdcbad1db536529591ebf9e9
Roos Cocolien <r.steigenga@gmail.com>
2021-10-19 17:38:52 +0200

    changed imports

--- pages/diary/index.js
+++ pages/diary/index.js
@@ -1,22 +1,24 @@
 import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
-import AppHeader from '../components/AppHeader'
+import AppHeader from '../../components/AppHeader'
 
 export default function Diary() {
     return(
-        <container className={styles.container}>
+        <div>
             <AppHeader />
-            <h1>Diary</h1>
-            <p>Check out the stories:</p>
-            <div className={styles.grid}>
-                <div>
-                    <a className={styles.card}>
-                        <Link href='/diary/first-date'>
-                                Story 1
-                        </Link>
-                    </a>
+            <div className={styles.container}>
+                <h1>Diary</h1>
+                <p>Check out the stories:</p>
+                <div className={styles.grid}>
+                    <div>
+                        <a className={styles.card}>
+                            <Link href='/diary/first-date'>
+                                    Story 1
+                            </Link>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </container>
+        </div>
     )
 }
